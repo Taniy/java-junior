33eb768d909daf762a50f550c4c11a871871a81d
Taniy <tanyadmp@mail.ru>
2015-10-29 16:21:02 +0300

    add javadoc

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,37 +1,55 @@
 package com.acme.edu;
 
 public class Logger {
+    public static final String PRIMITIVE = "primitive: ";
 //region fields
 
 //endregion
 
-
-
-
+    /**
+     * print for int and byte message
+     * @param message
+     */
     public static void log(int message) {
-        printer("primitive: " + message);
+        printer(PRIMITIVE + message);
     }
 
-    public static void log(byte message) {
-        printer("primitive: " + message);
-    }
-
+    /**
+     * print for boolean message
+     * @param message
+     */
     public static void log(boolean message) {
-        printer("primitive: " + message);
+        printer(PRIMITIVE + message);
     }
 
+    /**
+     * print for char message
+     * @param message
+     */
     public static void log(char message) {
         printer("char: " + message);
     }
 
+    /**
+     * print for string message
+     * @param message
+     */
     public static void log(String message) {
-        printer("string: "+message);
+        printer("string: " + message);
     }
 
-    public static void log(Object object) {
-        printer("reference: "+object);
+    /**
+     * print for object message
+     * @param message
+     */
+    public static void log(Object message) {
+        printer("reference: " + message);
     }
 
+    /**
+     * print message
+     * @param message
+     */
     public static void printer(String message) {
         System.out.println(message);
     }
