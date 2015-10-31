129b3ac43f4792483e9abbf0a9375324e1e91d00
Taniy <tanyadmp@mail.ru>
2015-10-31 17:36:50 +0300

    iteration3 ready

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,8 +1,9 @@
 package com.acme.edu;
 
 /**
- * at the end of using Logger
- * you should call function close()
+ * Log messages.
+ * At the end of using Logger
+ * you should call function close().
  */
 public class Logger {
     //region fields
@@ -10,14 +11,16 @@ public class Logger {
     public static final String SEP = System.lineSeparator();
     public static final String BRAKETOPEN = "{" + SEP;
     public static final String BRAKETCLOSE = "}" + SEP;
+    public static final int ZEROVALUE = 0;
+    public static final int SUMEMPTY = 0;
     private static int sumInt = 0;
     private static  int sumString = 0;
     private static String lastString = "";
     //endregion
 
     /**
-     * log for byte message
-     * @param message
+     * Log for byte message
+     * Print @param message
      */
     public static void log(byte message) {
         checkOnInt();
@@ -26,16 +29,16 @@ public class Logger {
     }
 
     /**
-     * log for int message
-     * @param message
+     * Log for int message
+     * Print @param message
      */
     public static void log(int message) {
         checkOnString();
-        if(message == 0 || message == Integer.MAX_VALUE) {
+        if(message == ZEROVALUE || message == Integer.MAX_VALUE) {
             checkOnInt();
             print(PRIMITIVE + message);
         } else {
-            if ((sumInt != 0) && (message > 0) && (sumInt > Integer.MAX_VALUE - message)) {
+            if ((sumInt != SUMEMPTY) && (message > 0) && (sumInt > Integer.MAX_VALUE - message)) {
                 print(PRIMITIVE + sumInt);
                 print(PRIMITIVE + message);
             } else {
@@ -45,8 +48,8 @@ public class Logger {
     }
 
     /**
-     * log for boolean message
-     * @param message
+     * Log for boolean message
+     * Print @param message
      */
     public static void log(boolean message) {
         checkOnInt();
@@ -55,8 +58,8 @@ public class Logger {
     }
 
     /**
-     * log for char message
-     * @param message
+     * Log for char message
+     * Print @param message
      */
     public static void log(char message) {
         checkOnInt();
@@ -65,8 +68,8 @@ public class Logger {
     }
 
     /**
-     * log for string message
-     * @param message
+     * Log for string message
+     * Print @param message
      */
     public static void log(String message) {
         checkOnInt();
@@ -80,8 +83,8 @@ public class Logger {
     }
 
     /**
-     * log for object message
-     * @param message
+     * Log for object message
+     * Print @param message
      */
     public static void log(Object message) {
         checkOnInt();
@@ -90,8 +93,8 @@ public class Logger {
     }
 
     /**
-     * log for array message
-     * @param message
+     * Log for array message
+     * Print @param message
      */
     public static void log(int... message) {
         int sum = 0;
@@ -102,14 +105,14 @@ public class Logger {
     }
 
     /**
-     * log for integerMatrix
-     * @param message
+     * Log for integerMatrix
+     * Print @param message
      */
     public static void log(int[][] message) {
         StringBuilder str = new StringBuilder();
         for(int i = 0; i < message.length;  i++) {
             str.append("{");
-            getString(message[i], str);
+            putInString(message[i], str);
             str.append(BRAKETCLOSE);
         }
         print("primitives matrix: " + BRAKETOPEN + str.toString() + "}");
@@ -117,8 +120,8 @@ public class Logger {
     }
 
     /**
-     * log for multiArray
-     * @param message
+     * Log for multiArray
+     * Print @param message
      */
     public static void log(int[][][][] message) {
         StringBuilder str = new StringBuilder();
@@ -128,7 +131,7 @@ public class Logger {
                 str.append(BRAKETOPEN);
                 for(int k = 0; k < message[i][j].length;  k++) {
                     str.append(BRAKETOPEN);
-                    getString(message[i][j][k], str);
+                    putInString(message[i][j][k], str);
                     str.append(SEP + BRAKETCLOSE);
                 }
                 str.append(BRAKETCLOSE);
@@ -140,8 +143,8 @@ public class Logger {
     }
 
     /**
-     * log for vararg
-     * @param message
+     * Log for vararg
+     * Print @param message
      */
     public static void log(String... message) {
         String str = "";
@@ -151,48 +154,39 @@ public class Logger {
         print(str);
     }
 
-
     /**
-     * close
-     * at the end of using class
+     * finish actions of logger
+     * write close() at the end of using class
      */
     public static void close() {
         checkOnInt();
         checkOnString();
     }
 
-    /**
-     * print message
-     * @param message
-     */
     private static void print(String message) {
         System.out.println(message);
     }
 
-    /**
-     * print sum of String that we have in buf
-     */
     private static void  checkOnString() {
-         if (sumString != 0) {
-             if (sumString == 1)
-                 print("string: " + lastString);
-             else
-                 print("string: " + lastString + " (x" + sumString + ")");
-             sumString = 0;
-         }
-    }
-
-    /**
-     * print sum of int that we have in buf
-     */
-    private static void  checkOnInt() {
-        if (sumInt != 0) {
-            print(PRIMITIVE + sumInt);
-            sumInt = 0;
+        if (sumString == SUMEMPTY)
+            return;
+        String str = "string: " + lastString;
+        if (sumString == 1) {
+            print(str);
+        } else {
+            print(str + " (x" + sumString + ")");
         }
+        sumString = SUMEMPTY;
     }
 
-    private static void getString( int[] ints,StringBuilder str) {
+    private static void  checkOnInt() {
+        if (sumInt == SUMEMPTY)
+            return;
+        print(PRIMITIVE + sumInt);
+        sumInt = SUMEMPTY;
+    }
+
+    private static void putInString(int[] ints, StringBuilder str) {
         for (int j = 0; j< ints.length; j++) {
             str.append(ints[j]);
             if (j != ints.length - 1)
